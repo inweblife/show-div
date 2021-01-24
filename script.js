9f3e54760078dc0e957c0c3bc5cc294d1f0fb5c9
Ivan Dimitrov <inweblife@yandex.com>
2021-01-24 15:53:26 +0200

    first coomit

---
+++ script.js
@@ -0,0 +1,6 @@
+var button = document.querySelector("button");
+var element = document.querySelector("main");
+
+  button.addEventListener('click', function(){
+  element.classList.toggle("second");
+});
